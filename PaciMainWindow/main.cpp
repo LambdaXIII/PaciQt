723667e2d575ed1703fc6ae5341ba5bd56a5e70e
xiii_1991 <xiii_1991@163.com>
2017-12-14 12:27:40 +0800

    application info setted

--- PaciMainWindow/main.cpp
+++ PaciMainWindow/main.cpp
@@ -3,6 +3,9 @@
 
 int main(int argc, char *argv[])
 {
+  QApplication::setApplicationName("Paci");
+  QApplication::setApplicationDisplayName(QApplication::translate("main", "啪词"));
+  QApplication::setApplicationVersion("0.1a");
   QApplication a(argc, argv);
   PaciMainWindow w;
   w.show();
